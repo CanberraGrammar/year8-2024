3e1ca618ff05258b357e83a8e586f639ae56cce4
Alexander Phillips <alexanderphillips@Alexanders-MacBook-Air-2.local>
2024-04-04 17:03:35 +1100

    Added Thursday Code

--- term-1/week09/thurs.py
+++ term-1/week09/thurs.py
@@ -1,3 +1,12 @@
 from machine import Pin
 import time
 button = Pin(0,Pin.IN, Pin.PULL_UP)
+led = Pin(1,Pin.OUT)
+previous = button.value()
+while True:
+    currentButton = button.value()
+    if currentButton == 0 and previous == 1:
+        led.toggle()
+    print(button.value())
+    time.sleep(0.01)
+    previous = currentButton
