e9a7c9ed50cf7f2000e8276417bdfd4d2baab819
Alexander Phillips <alexanderphillips@Alexanders-MacBook-Air-2.local>
2024-05-30 16:55:18 +1000

    Added Final Code

--- term-2/week05/wednesday.py
+++ term-2/week05/wednesday.py
@@ -2,12 +2,22 @@ from machine import *
 from ssd1306 import SSD1306_I2C
 import time
 
-#i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
-#oled = SSD1306_I2C(128, 64, i2c)
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
 
 potX = ADC(Pin(26))
 potY = ADC(Pin(27))
 
+x = int((potX.read_u16() / 65535) * 128)
+y = int((potY.read_u16() / 65535) * 64)
+
 while True:
-    print(potX.read_u16(),potY.read_u16())
+    px = x
+    py = y
+    x = int((potX.read_u16() / 65535) * 128)
+    y = int((potY.read_u16() / 65535) * 64)
+    print(x,y)
+    oled.line(x,y,px,py,1)
+    oled.show()
     time.sleep(0.01)
+    
