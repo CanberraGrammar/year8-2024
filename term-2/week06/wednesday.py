f5ec65f7d817bd0a26e88ac3452fefb58d52fd77
Alexander Phillips <alexanderphillips@Alexanders-MacBook-Air-2.local>
2024-06-13 15:18:09 +1000

    Uploaded Starter Code

--- term-2/week06/wednesday.py
+++ term-2/week06/wednesday.py
@@ -8,6 +8,10 @@ oled = SSD1306_I2C(128, 64, i2c)
 potX = ADC(Pin(26))
 potY = ADC(Pin(27))
 
+btn = Pin(2,Pin.IN,Pin.PULL_UP)
+
+lines = []
+
 x = int((potX.read_u16() / 65535) * 128)
 y = int((potY.read_u16() / 65535) * 64)
 
@@ -17,7 +21,17 @@ while True:
     x = int((potX.read_u16() / 65535) * 128)
     y = int((potY.read_u16() / 65535) * 64)
     print(x,y)
-    oled.line(x,y,px,py,1)
+    oled.fill(0)
+    if btn.value() == 1:
+        lines.append((x,y,px,py))
+        
+    for line in lines:
+        startX, startY, endX, endY = line
+        oled.line(startX, startY, endX, endY, 1)
+    oled.pixel(x-1,y,1-oled.pixel(x-1,y))
+    oled.pixel(x+1,y,1-oled.pixel(x+1,y))
+    oled.pixel(x,y-1,1-oled.pixel(x,y-1))
+    oled.pixel(x,y+1,1-oled.pixel(x,y+1))
     oled.show()
     time.sleep(0.01)
     
