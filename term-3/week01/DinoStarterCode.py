8b324f0fbfc56bdebb972d58fe623fbb74773759
Alexander Phillips <alexanderphillips@Alexanders-MacBook-Air-2.local>
2024-07-25 16:17:00 +1000

    Updated Code

--- term-3/week01/DinoStarterCode.py
+++ term-3/week01/DinoStarterCode.py
@@ -19,6 +19,25 @@ dinoAY = 0.15
 dinoWidth = 8
 dinoHeight = 16
  
+def drawFloor():
+    pass
+def drawDino():
+    pass
+def drawObstacles():
+    pass
+def drawScore():
+    pass
+def moveObstacles():
+    pass
+def generateObstacles():
+    pass
+def moveDino():
+    pass
+def checkIfHit():
+    pass
+def checkButtons():
+    pass
+
 while True:
  
     obstacles = [(74,40,8),(128,24,15)]
@@ -34,7 +53,16 @@ while True:
         starttime = time.ticks_ms()
         oled.fill(0)
  
-        # Call functions for the game here    
+        # Call functions for the game here
+        drawFloor()
+        drawDino()
+        drawObstacles()
+        drawScore()
+        moveObstacles()
+        generateObstacles()
+        moveDino()
+        checkIfHit()
+        checkButtons()
  
         oled.show()    
         endtime = time.ticks_ms()
